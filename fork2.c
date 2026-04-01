adabe7f3055746b49acd8a639d65d351f23b58c0
ozlem <ozlem.demir@eresense.com>
2026-04-01 16:11:46 +0300

    uygulamadan değiştirdim

--- fork2.c
+++ fork2.c
@@ -4,3 +4,4 @@ https://www.youtube.com/watch?v=VfDGDpNkQps
 https://www.youtube.com/watch?v=VfDGDpNkQps
 https://www.youtube.com/watch?v=VfDGDpNkQps
 https://www.youtube.com/watch?v=VfDGDpNkQps
+ekleme saati : 16:11
